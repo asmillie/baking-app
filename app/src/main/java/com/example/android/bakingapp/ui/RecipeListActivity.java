46ecdca1b0198f9460f2fa17348100eef436270d
Alan Smillie <alan.smillie@gmail.com>
2018-11-22 15:20:56 -0500

    Gradle dependencies

--- app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
@@ -1,7 +1,9 @@
-package com.example.android.bakingapp;
+package com.example.android.bakingapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.android.bakingapp.R;
 
 public class RecipeListActivity extends AppCompatActivity {
 
