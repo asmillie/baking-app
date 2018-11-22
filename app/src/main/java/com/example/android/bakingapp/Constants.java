a152df4d9aecd85e848e16cc6a20995e428e659e
Alan Smillie <alan.smillie@gmail.com>
2018-11-22 15:28:17 -0500

    Create constants class, add path to json recipe list

--- app/src/main/java/com/example/android/bakingapp/Constants.java
+++ app/src/main/java/com/example/android/bakingapp/Constants.java
@@ -1,4 +1,9 @@
 package com.example.android.bakingapp;
 
 public class Constants {
+
+    //Prevent instantiation
+    private Constants() {}
+
+    public static final String RECIPE_LIST_JSON_PATH = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 }
