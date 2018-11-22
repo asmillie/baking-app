23865708f37ccbe18ccb21be8c7341000961a7a1
Alan Smillie <alan.smillie@gmail.com>
2018-11-22 15:42:41 -0500

    Add constructor with initial retrofit code

--- app/src/main/java/com/example/android/bakingapp/data/AppRepository.java
+++ app/src/main/java/com/example/android/bakingapp/data/AppRepository.java
@@ -1,5 +1,19 @@
 package com.example.android.bakingapp.data;
 
+import com.example.android.bakingapp.Constants;
+
+import retrofit2.Retrofit;
+
 public class AppRepository {
-    
+
+    private RecipeListService mRecipeListService;
+    //TODO: Continue implementing Retrofit to retrieve JSON list of recipes (refer to popular movies project)
+    public AppRepository() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.RECIPE_LIST_JSON_PATH)
+                .build();
+
+        mRecipeListService = retrofit.create(RecipeListService.class);
+    }
 }
