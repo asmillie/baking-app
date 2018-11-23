a953ca86488fbc7207a90564746ee4edcd0f7356
Alan Smillie <alan.smillie@gmail.com>
2018-11-23 10:04:10 -0500

    Observe view model

--- app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
@@ -1,15 +1,37 @@
 package com.example.android.bakingapp.ui;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.data.Recipe;
+
+import java.util.List;
 
 public class RecipeListActivity extends AppCompatActivity {
+
+    private RecipeListViewModel mViewModel;
+    private List<Recipe> mRecipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        initViewModel();
+    }
+
+    private void initViewModel() {
+        mViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+
+        mViewModel.getRecipeList().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(@Nullable List<Recipe> recipes) {
+                mRecipeList = recipes;
+            }
+        });
     }
 }
