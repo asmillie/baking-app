043fd19eabd90a1cada0115616f336bd12f97058
Alan Smillie <alan.smillie@gmail.com>
2018-11-23 10:00:49 -0500

    Create RecipeList ViewModel

---
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeListViewModel.java
@@ -0,0 +1,29 @@
+package com.example.android.bakingapp.ui;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.example.android.bakingapp.data.AppRepository;
+import com.example.android.bakingapp.data.Recipe;
+
+import java.util.List;
+
+public class RecipeListViewModel extends AndroidViewModel {
+
+    private final AppRepository mRepository;
+    private LiveData<List<Recipe>> mRecipeList;
+
+    public RecipeListViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = AppRepository.getInstance(application.getApplicationContext());
+    }
+
+    LiveData<List<Recipe>> getRecipeList() {
+        if (mRecipeList == null) {
+            mRecipeList = mRepository.getRecipes();
+        }
+        return mRecipeList;
+    }
+}
