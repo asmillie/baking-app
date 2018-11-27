86e09398b8519bb0dc42373b10170df7c32b4b70
Alan Smillie <alan.smillie@gmail.com>
2018-11-27 13:36:49 -0500

    Set basic recipe item list layout

--- app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
@@ -5,6 +5,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.example.android.bakingapp.R;
@@ -17,19 +19,25 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RecipeListActivity extends AppCompatActivity {
+public class RecipeListActivity extends AppCompatActivity implements RecipeListAdapter.RecipeClickListener {
 
     private RecipeListViewModel mViewModel;
     private List<RecipeAndInstructions> mRecipeList;
+
+    private RecipeListAdapter mAdapter;
+
+    @BindView(R.id.recipe_list_rv)
+    RecyclerView mRecipeListRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
-        initViewModel();
-
         ButterKnife.bind(this);
+
+        initViewModel();
+        initUI();
     }
 
     private void initViewModel() {
@@ -39,10 +47,19 @@ public class RecipeListActivity extends AppCompatActivity {
             @Override
             public void onChanged(@Nullable List<RecipeAndInstructions> recipeAndInstructions) {
                 mRecipeList = recipeAndInstructions;
+                mAdapter.setRecipeList(mRecipeList);
             }
         });
+    }
 
+    private void initUI() {
+        mAdapter = new RecipeListAdapter(mRecipeList, this);
 
+        mRecipeListRV.setHasFixedSize(true);
+        mRecipeListRV.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        );
+        mRecipeListRV.setAdapter(mAdapter);
     }
 
     private void clearUI() {
@@ -51,5 +68,10 @@ public class RecipeListActivity extends AppCompatActivity {
 
     private void populateUI() {
 
+    }
+
+    @Override
+    public void onRecipeClick(int recipeId) {
+        //TODO intent to open detail view
     }
 }
