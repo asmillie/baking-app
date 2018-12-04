67fdcd9309ba1364ce3dfcd828b5a62c6ed5b793
Alan Smillie <alan.smillie@gmail.com>
2018-12-04 14:18:34 -0500

    Add grid layout for larger screens

--- app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/RecipeListActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.widget.TextView;
 
 import com.example.android.bakingapp.Constants;
@@ -18,6 +20,7 @@ import com.example.android.bakingapp.data.RecipeAndInstructions;
 
 import java.util.List;
 
+import butterknife.BindBool;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -31,6 +34,7 @@ public class RecipeListActivity extends AppCompatActivity implements RecipeListA
     @BindView(R.id.recipe_list_rv)
     RecyclerView mRecipeListRV;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,20 @@ public class RecipeListActivity extends AppCompatActivity implements RecipeListA
 
         initViewModel();
         initUI();
+    }
+
+    /**
+     * Solution to determining screen density found
+     * @ https://stackoverflow.com/questions/6465680/how-to-determine-the-screen-width-in-terms-of-dp-or-dip-at-runtime-in-android
+     */
+    private boolean isLargeScreen() {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+        if (dpWidth >= 600) {
+            return true;
+        }
+        return false;
     }
 
     private void initViewModel() {
@@ -58,9 +76,15 @@ public class RecipeListActivity extends AppCompatActivity implements RecipeListA
         mAdapter = new RecipeListAdapter(mRecipeList, this);
 
         mRecipeListRV.setHasFixedSize(true);
-        mRecipeListRV.setLayoutManager(
-                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        );
+
+        RecyclerView.LayoutManager layoutManager;
+        if (isLargeScreen()) {
+            layoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        } else {
+            layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        }
+
+        mRecipeListRV.setLayoutManager(layoutManager);
         mRecipeListRV.setAdapter(mAdapter);
     }
 
