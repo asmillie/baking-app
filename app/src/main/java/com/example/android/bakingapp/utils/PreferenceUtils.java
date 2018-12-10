1e3a9bf8f5a0b8bb9bd05ea7edc4ae10825dcaa4
Alan Smillie <alan.smillie@gmail.com>
2018-12-10 11:24:05 -0500

    Create utils class for shared prefs methods

---
+++ app/src/main/java/com/example/android/bakingapp/utils/PreferenceUtils.java
@@ -0,0 +1,17 @@
+package com.example.android.bakingapp.utils;
+
+public class PreferenceUtils {
+
+    private PreferenceUtils() {}
+
+    public static Integer getWidgetRecipeId(int widgetId) {
+
+        //TODO Check sharedPrefs for selected recipe id for provided widgetId
+
+        return -1;
+    }
+
+    public static void setWidgetRecipeId(int widgetId, Integer recipeId) {
+        //TODO Save selected recipe id for widget
+    }
+}
