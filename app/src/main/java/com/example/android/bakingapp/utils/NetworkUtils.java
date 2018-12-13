0bab79f971e8ede8d00224e5bc2e8745d7215c04
Alan Smillie <alan.smillie@gmail.com>
2018-12-13 10:09:49 -0500

    Add networkutils class

---
+++ app/src/main/java/com/example/android/bakingapp/utils/NetworkUtils.java
@@ -0,0 +1,30 @@
+package com.example.android.bakingapp.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+public class NetworkUtils {
+
+    /** Tag for Error Logging **/
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private NetworkUtils() {}
+
+    public static Boolean isConnected(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork;
+        boolean isConnected = false;
+        try {
+            activeNetwork = cm.getActiveNetworkInfo();
+            isConnected = activeNetwork != null && activeNetwork.isConnected();
+        } catch (NullPointerException e) {
+            Log.e(TAG, "Error retrieving active network info");
+        }
+
+        return isConnected;
+    }
+}
